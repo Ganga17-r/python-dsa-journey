5832446e89eaafa2865a8a6f2ec7a7eb180f379b
unknown <gangambika115@gmail.com>
2026-07-25 09:57:54 +0530

    Day 4: Python comparison operators and array traversal practice

---
+++ OneDrive/Desktop/python-dsa-journey/python/python_day4_comparison_operators.py
@@ -0,0 +1,20 @@
+# Day 4 - Comparison Operators
+
+a = 10
+b = 20
+
+print("a == b :", a == b)   # Equal to
+print("a != b :", a != b)   # Not Equal to
+print("a > b  :", a > b)    # Greater than
+print("a < b  :", a < b)    # Less than
+print("a >= b :", a >= b)   # Greater than or Equal to
+print("a <= b :", a <= b)   # Less than or Equal to
+a = int(input("enter you first number:"))
+b = int(input("enter your second number:"))
+
+print("a == b :", a == b)   # Equal to
+print("a != b :", a != b)   # Not Equal to
+print("a > b  :", a > b)    # Greater than
+print("a < b  :", a < b)    # Less than
+print("a >= b :", a >= b)   # Greater than or Equal to
+print("a <= b :", a <= b)   # Less than or Equal to8
