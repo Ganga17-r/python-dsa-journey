3317547c9b9caf15341ab14b23317abec5389e7c
unknown <gangambika115@gmail.com>
2026-08-06 17:57:37 +0530

    DSA Day 10 Revision: Solved Q7 and Q8 array traversal practice

---
+++ OneDrive/Desktop/python-dsa-journey/DSA/Day-10_Revision/q7.py
@@ -0,0 +1,5 @@
+# Print only the values less than 5.
+arr = [2, 3, 4, 5, 6]
+for i in range(len(arr)):
+    if arr[i]<5:
+        print(arr[i])
---
+++ OneDrive/Desktop/python-dsa-journey/DSA/Day-10_Revision/q8.py
@@ -0,0 +1,5 @@
+# Print only the values greater than or equal to 5.
+arr=[2,3,4,5,6,7]
+for i in range(len(arr)):
+    if arr[i]>=5:
+        print(arr[i])
